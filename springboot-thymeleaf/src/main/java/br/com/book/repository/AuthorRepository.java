a6a868f7f9530f5086325b56f777c141344c131d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-12-02 08:12:58 -0300

    Delete springboot-thymeleaf directory

--- springboot-thymeleaf/src/main/java/br/com/book/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.book.repository;
-
-import br.com.book.entity.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
