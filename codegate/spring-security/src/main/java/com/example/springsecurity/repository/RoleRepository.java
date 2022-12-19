b3c07225d60387267a6a7de1d251dec493d379cf
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-12-19 10:58:35 -0300

    Delete codegate/spring-security directory

--- codegate/spring-security/src/main/java/com/example/springsecurity/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springsecurity.repository;
-
-import com.example.springsecurity.model.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-    Role findByRole(String role);
-}
--- codegate/spring-security/src/main/java/com/example/springsecurity/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springsecurity.repository;
-
-import com.example.springsecurity.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository <User, Long> {
-    User findByUsername(String username);
-}
